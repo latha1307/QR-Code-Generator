e7d86e3ee2333c25beb6f516f791c239d7221870
latha1307 <ldkannammal@gmail.com>
2024-04-14 19:52:50 +0530

    QRCode Generator app is updated

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QrCode } from './QrCode'
+import './QrCode.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QrCode/>
+  </React.StrictMode>,
+)
